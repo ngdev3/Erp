24399e5ec9309b53fb21508890f69ba430d4af46
Er Rajat Gupta <45349777+ngdev3@users.noreply.github.com>
2024-02-04 18:03:38 +0530

    Update head_ref.php

--- application/views/content/head_ref.php
+++ application/views/content/head_ref.php
@@ -17,12 +17,19 @@
 <!-- Jquery Confirm Box -->
 <link rel="stylesheet" href="<?php echo base_url(); ?>plugins/jconfirm/jquery-confirm.min.css">
 
+
+
 <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
 <script>
-  window.OneSignalDeferred = window.OneSignalDeferred || [];
-  OneSignalDeferred.push(function(OneSignal) {
-    OneSignal.init({
-      appId: "865a79ce-2666-43f3-b105-4ded58b35a78",
+    window.OneSignalDeferred = window.OneSignalDeferred || [];
+    window.OneSignalDeferred.push(async function(OneSignal) {
+        await OneSignal.init({
+            appId: "865a79ce-2666-43f3-b105-4ded58b35a78",
+            notifyButton: {
+                enable: true
+            },
+            serviceWorkerParam: { scope: "https://thecrindustries.online/push/onesignal/" },
+            serviceWorkerPath: "https://thecrindustries.online/push/onesignal/OneSignalSDKWorker.js",
+        });
     });
-  });
 </script>
