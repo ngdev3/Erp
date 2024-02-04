1e521632cde7e0097d99c8597f95dfc40350d0e0
Er Rajat Gupta <45349777+ngdev3@users.noreply.github.com>
2024-02-04 18:01:11 +0530

    SDK

--- application/views/content/head_ref.php
+++ application/views/content/head_ref.php
@@ -16,3 +16,13 @@
 
 <!-- Jquery Confirm Box -->
 <link rel="stylesheet" href="<?php echo base_url(); ?>plugins/jconfirm/jquery-confirm.min.css">
+
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(function(OneSignal) {
+    OneSignal.init({
+      appId: "865a79ce-2666-43f3-b105-4ded58b35a78",
+    });
+  });
+</script>
