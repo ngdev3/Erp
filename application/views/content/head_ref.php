83bcb87cbe38d6e5ce5b9fda443b960edc1abc42
Rajat Gupta <rajatinvoice@gmail.com>
2024-03-24 11:23:57 +0530

    Update head_ref.php

--- application/views/content/head_ref.php
+++ application/views/content/head_ref.php
@@ -16,20 +16,3 @@
 
 <!-- Jquery Confirm Box -->
 <link rel="stylesheet" href="<?php echo base_url(); ?>plugins/jconfirm/jquery-confirm.min.css">
-
-
-
-<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-<script>
-    window.OneSignalDeferred = window.OneSignalDeferred || [];
-    window.OneSignalDeferred.push(async function(OneSignal) {
-        await OneSignal.init({
-            appId: "865a79ce-2666-43f3-b105-4ded58b35a78",
-            notifyButton: {
-                enable: true
-            },
-            serviceWorkerParam: { scope: "https://thecrindustries.online/push/onesignal/" },
-            serviceWorkerPath: "https://thecrindustries.online/push/onesignal/OneSignalSDKWorker.js",
-        });
-    });
-</script>
